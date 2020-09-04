3c73b1f37ad53f7e215a27e23aadde2f43c3d8d7
justinhunt <bitwalkerjapan@gmail.com>
2020-09-04 21:44:29 +0900

    uu

--- classes/common.php
+++ classes/common.php
@@ -300,7 +300,7 @@ class common
         $newshortname=$company['shortname'];
         if ($DB->get_record('company', array('shortname' =>  $newshortname))) {
             for($suffixcount=1;$suffixcount<21;$suffixcount++) {
-                $newshortname =\self::truncate_string($company['shortname'],22)  . '_' . $suffixcount;
+                $newshortname =self::truncate_string($company['shortname'],22)  . '_' . $suffixcount;
                 if (!$DB->get_record('company', array('shortname' => $newshortname))) {
                     $company['shortname']=$newshortname;
                     break;
