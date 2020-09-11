182c6802ebd2c62a68ab629a5d3a0f5f3b105640
justinhunt <bitwalkerjapan@gmail.com>
2020-09-11 23:24:40 +0900

    iii

--- classes/common.php
+++ classes/common.php
@@ -954,11 +954,12 @@ class common
                 'Authorization: Basic '. base64_encode($username.':'),
         );
         if(!empty($username)) {
-         //   echo 'YES USERNAME';
+            echo 'YES USERNAME';
             $curl->setopt('CURLOPT_HTTPAUTH', CURLAUTH_BASIC);
             $curl->setopt('CURLOPT_HTTPHEADER', $headers);
             //$curl->setopt(array('CURLOPT_USERPWD'=> $username . ":"));
         }
+        echo "username ???";
         $result = $curl->post($url, $postdata);
         return $result;
     }
