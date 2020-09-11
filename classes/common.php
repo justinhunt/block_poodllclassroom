0c41f8e5400d5a12319366cc733e1196f1557c9b
justinhunt <bitwalkerjapan@gmail.com>
2020-09-11 23:44:02 +0900

    iii

--- classes/common.php
+++ classes/common.php
@@ -883,7 +883,7 @@ class common
             $url = "https://$siteprefix.chargebee.com/api/v2/portal_sessions";
             $postdata=[];
             $postdata['redirect_url'] = $CFG->wwwroot . '/my';
-            $postdata['customer']= http_build_query((array("id" => "$customerid")));
+            $postdata['customer[id]']=  "$customerid";
             $curlresult = self::curl_fetch($url,$postdata,$apikey);
             $jsonresult = self::make_object_from_json($curlresult);
             if($jsonresult){
