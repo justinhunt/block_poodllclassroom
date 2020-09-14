00fe6677485599bc653fe8bc97e348e80e5f3100
justinhunt <bitwalkerjapan@gmail.com>
2020-09-14 20:56:43 +0900

    i

--- amd/src/chargebeehelper.js
+++ amd/src/chargebeehelper.js
@@ -1,5 +1,5 @@
 /* jshint ignore:start */
-define(['jquery','core/log','core/ajax','https://js.chargebee.com/v2/chargebee.js'], function($, log, ajax, chargebee) {
+define(['jquery','core/log','core/ajax',], function($, log, ajax, chargebee) {
 
     "use strict";
     log.debug('cbhelper: initialising');
@@ -19,49 +19,57 @@ define(['jquery','core/log','core/ajax','https://js.chargebee.com/v2/chargebee.j
         },
 
         register_events: function() {
-            $("." + this.changeplanclass).on("click", function() {
-                event.preventDefault();
-                chargebee.openCheckout({
-                    hostedPage: function() {
-                        // Hit your end point that returns hosted page object as response
-                        // This sample end point will call checkout new api
-                        // https://apidocs.chargebee.com/docs/api/hosted_pages#checkout_new_subscription
-                        // If you want to use paypal, go cardless and plaid, pass embed parameter as false
-                        // Now we can continue...
-                        var promises = ajax.call([{
-                            methodname: 'block_poodllclassroom_get_checkout_existing',
-                            args: {}
-                        }]);
-                        return promises[0];
-                       /*
-                        return $.ajax({
-                            method: "post",
-                            url: "http://localhost:8000/api/generate_checkout_existing_url"
-                        });
-                        */
-                    },
-                    loaded: function() {
-                        console.log("checkout opened");
-                    },
-                    error: function() {
-                    },
-                    close: function() {
-                        console.log("checkout closed");
-                    },
-                    success: function(hostedPageId) {
-                        console.log(hostedPageId);
-                        // Hosted page id will be unique token for the checkout that happened
-                        // You can pass this hosted page id to your backend
-                        // and then call our retrieve hosted page api to get subscription details
-                        // https://apidocs.chargebee.com/docs/api/hosted_pages#retrieve_a_hosted_page
-                    },
-                    step: function(value) {
-                        // value -> which step in checkout
-                        console.log(value);
-                    }
-                });
-            });
-        }
+
+            $.getScript('https://js.chargebee.com/v2/chargebee.js', function(){
+                var chargebee = Chargebee.getInstance();
+                $("." + this.changeplanclass).on("click", function() {
+                    event.preventDefault();
+                    chargebee.openCheckout({
+                        hostedPage: function() {
+                            // Hit your end point that returns hosted page object as response
+                            // This sample end point will call checkout new api
+                            // https://apidocs.chargebee.com/docs/api/hosted_pages#checkout_new_subscription
+                            // If you want to use paypal, go cardless and plaid, pass embed parameter as false
+                            // Now we can continue...
+                            var promises = ajax.call([{
+                                methodname: 'block_poodllclassroom_get_checkout_existing',
+                                args: {}
+                            }]);
+                            return promises[0];
+                            /*
+                             return $.ajax({
+                                 method: "post",
+                                 url: "http://localhost:8000/api/generate_checkout_existing_url"
+                             });
+                             */
+                        },
+                        loaded: function() {
+                            console.log("checkout opened");
+                        },
+                        error: function() {
+                        },
+                        close: function() {
+                            console.log("checkout closed");
+                        },
+                        success: function(hostedPageId) {
+                            console.log(hostedPageId);
+                            // Hosted page id will be unique token for the checkout that happened
+                            // You can pass this hosted page id to your backend
+                            // and then call our retrieve hosted page api to get subscription details
+                            // https://apidocs.chargebee.com/docs/api/hosted_pages#retrieve_a_hosted_page
+                        },
+                        step: function(value) {
+                            // value -> which step in checkout
+                            console.log(value);
+                        }
+                    });
+                });//on click
+
+            });//end of get script
+
+
+
+        }//end of reg events
     };//end of returned object
 });//total end
 
