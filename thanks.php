e464b92aa957d0cd45eefcc2047a87d5b543ec51
justinhunt <bitwalkerjapan@gmail.com>
2020-09-27 14:36:30 +0900

    ii

--- thanks.php
+++ thanks.php
@@ -25,10 +25,9 @@
 
 require('../../config.php');
 use block_poodllclassroom\constants;
-
-echo $OUTPUT->header();
 $PAGE->set_context(\context_system::instance());
 $PAGE->set_url(constants::M_URL . '/thanks.php');
+echo $OUTPUT->header();
 $thanksdata = array('dashboardurl'=>$CFG->wwwroot . '/my');
 echo $OUTPUT->render_from_template('block_poodllclassroom/thanks',$thanksdata );
 echo $OUTPUT->footer();
