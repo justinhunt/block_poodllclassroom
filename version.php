b173535c5854987adfd6d1e1338c4e660ff549bc
Justin Hunt <bitwalkerjapan@gmail.com>
2026-02-02 15:45:53 +0900

    indexes for subs table

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022062900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2026020200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_poodllclassroom'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.51(Build 2022062900)';
+$plugin->release   = '1.0.52(Build 2026020200)';
